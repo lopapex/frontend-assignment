8cbf64ca79e8156651a3234c3074fc1a62f5c3ed
Matěj Novotný <novotny@webscope.io>
2024-10-24 19:51:02 +0200

    chore: generate api files

---
+++ src/api/generated/models/index.ts
@@ -0,0 +1,6 @@
+export * from './InlineObject';
+export * from './InlineResponse200';
+export * from './TodoRequest';
+export * from './TodoResponse';
+export * from './Tokens';
+export * from './User';
