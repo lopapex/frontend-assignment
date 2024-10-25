d90fd23bc468ee2fa4d840d541e0431e75bab4d3
Matěj Novotný <novotny@webscope.io>
2024-10-25 11:38:58 +0200

    feat: add margin to content card

--- src/components/ui/ContentCard.tsx
+++ src/components/ui/ContentCard.tsx
@@ -4,7 +4,7 @@ import {PropsWithChildren} from 'react';
 type ContentCardProps = BoxProps & PropsWithChildren;
 
 export const ContentCard = ({children, ...props}: ContentCardProps) => (
-  <Box bg="fill-white" padding="10" borderRadius="xl" {...props}>
+  <Box bg="fill-white" padding="10" margin="8px" borderRadius="xl" {...props}>
     {children}
   </Box>
 );
